1265eecff56e5f98d06d7b15c2451e827fb65a12
Alim Zanibekov <alim.zanibekov@intspirit.com>
2020-06-28 21:44:24 +0300

    Refactor video player

--- lib/app/extensions/video/player.dart
+++ lib/app/extensions/video/player.dart
@@ -21,186 +21,192 @@ class AppVideoPlayer extends StatefulWidget {
 
 class _AppVideoPlayerState extends State<AppVideoPlayer> {
   static List<VideoPlayerController> _cache = [];
-  StreamController<int> _streamController;
   VideoPlayerController _controller;
   bool _isPlaying = false;
-  bool _initialized = false;
   bool _controllerDestroyed = false;
+  bool _initialized = false;
+  bool _hasError = false;
 
   @override
   void initState() {
-    _streamController = StreamController();
     _load();
     super.initState();
   }
 
   @override
   void dispose() {
+    _disposeController();
+    super.dispose();
+  }
+
+  void _notify() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _disposeController() {
     try {
       final element = _cache.firstWhere((element) => element == _controller);
       _cache.remove(element);
     } on StateError {}
+
     if (_controller != null) {
       _controller.removeListener(_onChangeVideoState);
       _controller.dispose();
+      _controller = null;
     }
-    _streamController.close();
-    super.dispose();
+  }
+
+  void _removeFromCacheAndNotify(VideoPlayerController ctrl) {
+    if (_cache.length >= 9) {
+      _cache.remove(ctrl);
+      ctrl.setLooping(false);
+    }
+  }
+
+  Future _safeRemoveController() async {
+    await Future.delayed(Duration(milliseconds: 30));
+    _notify();
+    await Future.delayed(Duration(milliseconds: 100));
+    _disposeController();
+    _notify();
   }
 
   void _load() {
+    _initialized = false;
+    _hasError = false;
+
     if (_controllerDestroyed) {
       _controllerDestroyed = false;
-      if (_cache.length >= 9) {
-        final last = _cache.last;
-        _cache.remove(last);
-        last.setLooping(false);
-      }
-    } else if (_cache.length >= 9) {
-      final first = _cache.first;
-      _cache.removeAt(0);
-      first.setLooping(false);
+      _removeFromCacheAndNotify(_cache.isNotEmpty ? _cache.last : null);
+    } else {
+      _removeFromCacheAndNotify(_cache.isNotEmpty ? _cache.first : null);
     }
-    _controller = VideoPlayerController.network(widget.url,
+
+    if (mounted) {
+      _controller = VideoPlayerController.network(
+        widget.url,
         httpHeaders: REACTOR_VIDEO_HEADERS,
         maxCacheSize: 200 << 20,
-        maxFileSize: 10 << 20)
-      ..initialize()
-      ..setLooping(true).then((_) {
-        if (mounted) {
-          setState(() {});
-        }
-      });
-    _cache.add(_controller);
+        maxFileSize: 10 << 20,
+      )
+        ..setLooping(true)
+        ..initialize();
 
-    _controller.addListener(_onChangeVideoState);
-    _initialized = _controller.value.initialized;
-    setState(() {});
+      _cache.add(_controller);
+      _controller.addListener(_onChangeVideoState);
+      _initialized = _controller.value.initialized;
+
+      _notify();
+    }
   }
 
-  void _onChangeVideoState() {
+  void _onChangeVideoState() async {
     if (_controllerDestroyed) {
       return;
     }
     if (!_cache.contains(_controller)) {
       _controllerDestroyed = true;
       _initialized = false;
-      if (_streamController.hasListener) {
-        _streamController.add(1);
-        Future.delayed(Duration(milliseconds: 200)).then((value) {
-          _controller.removeListener(_onChangeVideoState);
-          _controller.dispose();
-          _controller = null;
-
-          if (_streamController.hasListener) {
-            _streamController.add(1);
-          }
-        });
-      }
+      await _safeRemoveController();
       return;
     }
 
     if (_isPlaying != _controller.value.isPlaying) {
       _isPlaying = _controller.value.isPlaying;
-      if (_streamController.hasListener) {
-        _streamController.add(1);
-      }
+      _notify();
     }
+
     if (_initialized != _controller.value.initialized) {
       _initialized = _controller.value.initialized;
-      if (_streamController.hasListener) {
-        _streamController.add(1);
-      }
+      _notify();
     }
+
     if (_controller.value.hasError) {
-      _controller.dispose();
-      if (_streamController.hasListener) {
-        _streamController.add(1);
-      }
+      _hasError = _controller.value.hasError;
+      await _safeRemoveController();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: _streamController.stream,
-      builder: (_, __) {
-        if (_controllerDestroyed || _controller == null) {
-          return AspectRatio(
-            aspectRatio: widget.aspectRatio < 2 ? widget.aspectRatio : 2,
-            child: AppOnErrorReload(
-              icon: const Icon(Icons.error_outline, size: 44),
-              text: 'Данный объект был выгружен из памяти',
-              onReloadPressed: () {
-                _load();
-              },
-            ),
-          );
-        }
-        if (_controller.value.hasError) {
-          return AspectRatio(
-            aspectRatio: widget.aspectRatio < 2 ? widget.aspectRatio : 2,
-            child: AppOnErrorReload(
-              text: 'Произошла ошибка при загруке',
-              onReloadPressed: () {
-                _load();
-              },
-            ),
-          );
-        }
-        return AspectRatio(
-          aspectRatio: widget.aspectRatio < 2 ? widget.aspectRatio : 2,
-          key: ValueKey(widget.url),
-          child: _initialized
-              ? Stack(children: <Widget>[
-                  ClipRect(child: VideoPlayer(_controller)),
-                  GestureDetector(
-                    onTap: () {
-                      if (!_isPlaying) {
-                        _controller.play();
-                      } else {
-                        _controller.pause();
-                      }
-                    },
-                    child: AnimatedOpacity(
-                        duration: const Duration(milliseconds: 150),
-                        opacity: _isPlaying ? 0 : 1,
-                        child: const DecoratedBox(
-                            decoration: BoxDecoration(
-                                color: Color.fromRGBO(0, 0, 0, 0.5)),
-                            child: Center(
-                              child: Icon(Icons.play_arrow,
-                                  size: 42, color: Colors.white),
-                            ))),
-                  ),
-                  if (_isPlaying)
-                    Positioned(
-                      right: 0,
-                      top: 0,
-                      child: GestureDetector(
-                        onTap: () {
-                          _controller.seekTo(Duration.zero);
-                          _controller.play();
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          padding: const EdgeInsets.only(
-                              top: 10, right: 10, bottom: 20, left: 20),
-                          child: IconShadowWidget(
-                            const Icon(Icons.refresh,
-                                size: 22, color: Colors.white),
-                            shadowColor: Colors.black,
-                          ),
-                        ),
+    if (_controllerDestroyed) {
+      return AspectRatio(
+        aspectRatio: widget.aspectRatio < 2 ? widget.aspectRatio : 2,
+        child: AppOnErrorReload(
+          icon: const Icon(Icons.error_outline, size: 44),
+          text: 'Данный объект был выгружен из памяти',
+          onReloadPressed: () => _load(),
+        ),
+      );
+    }
+    if (_hasError) {
+      return AspectRatio(
+        aspectRatio: widget.aspectRatio < 2 ? widget.aspectRatio : 2,
+        child: AppOnErrorReload(
+          text: 'Произошла ошибка при загруке',
+          onReloadPressed: () => _load(),
+        ),
+      );
+    }
+    return AspectRatio(
+      aspectRatio: widget.aspectRatio < 2 ? widget.aspectRatio : 2,
+      key: ValueKey(widget.url),
+      child: _initialized
+          ? Stack(children: <Widget>[
+              ClipRect(child: VideoPlayer(_controller)),
+              GestureDetector(
+                onTap: () {
+                  if (!_isPlaying) {
+                    _controller.play();
+                  } else {
+                    _controller.pause();
+                  }
+                },
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 150),
+                  opacity: _isPlaying ? 0 : 1,
+                  child: const DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(0, 0, 0, 0.5),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.play_arrow,
+                        size: 42,
+                        color: Colors.white,
                       ),
                     ),
-                ])
-              : FadeIcon(
-                  key: ObjectKey(widget.url),
-                  icon: Icon(Icons.gif, color: Colors.grey[500], size: 44),
+                  ),
                 ),
-        );
-      },
+              ),
+              if (_isPlaying)
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      _controller.seekTo(Duration.zero);
+                      _controller.play();
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      padding: const EdgeInsets.only(
+                          top: 10, right: 10, bottom: 20, left: 20),
+                      child: IconShadowWidget(
+                        const Icon(Icons.refresh,
+                            size: 22, color: Colors.white),
+                        shadowColor: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+            ])
+          : FadeIcon(
+              key: ObjectKey(widget.url),
+              icon: Icon(Icons.gif, color: Colors.grey[500], size: 44),
+            ),
     );
   }
 }
